074068e3353bbbddb38b68af94e8b6e25d0947ec
JorshBG <jordiyair29@gmail.com>
2024-07-26 20:53:46 -0600

    Create loader service of faker data

---
+++ src/main/java/com/jorshbg/practiceapispring/service/LoadFakeDataService.java
@@ -0,0 +1,80 @@
+package com.jorshbg.practiceapispring.service;
+
+import com.github.javafaker.Faker;
+import com.jorshbg.practiceapispring.model.Comment;
+import com.jorshbg.practiceapispring.model.Post;
+import com.jorshbg.practiceapispring.model.User;
+import com.jorshbg.practiceapispring.repository.CommentRepository;
+import com.jorshbg.practiceapispring.repository.PostRepository;
+import com.jorshbg.practiceapispring.repository.UserRepository;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+@Service
+public class LoadFakeDataService {
+
+    @Autowired
+    private PasswordEncoder encoder;
+
+    private Faker faker = new Faker();
+    private Random random = new Random();
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @Autowired
+    private CommentRepository commentRepository;
+
+    @PostConstruct
+    @Transactional
+    public void init(){
+        userRepository.deleteAll();
+        postRepository.deleteAll();
+        commentRepository.deleteAll();
+
+        List<User> users = new ArrayList<>();
+        List<Post> posts = new ArrayList<>();
+
+        for (int i = 0; i < 20; i++) {
+            User user = new User();
+            user.setUsername(faker.name().username());
+            user.setFirstName(faker.name().firstName());
+            user.setLastName(faker.name().lastName());
+            user.setEmail(faker.internet().safeEmailAddress());
+            user.setPassword(encoder.encode(faker.internet().password()));
+            user.setBiography(faker.lorem().sentence());
+            user.setBirthday(LocalDate.of(random.nextInt(1980, 2010), random.nextInt(1, 12), random.nextInt(1, 28)));
+            user.setPhone(faker.phoneNumber().cellPhone());
+            user.setRememberToken(faker.lorem().sentence());
+            users.add(userRepository.save(user));
+        }
+
+        for (int i = 0; i < 200; i++) {
+            Post post = new Post();
+            post.setContent(faker.lorem().paragraph(40));
+            post.setTitle(faker.lorem().sentence(5));
+            post.setAuthor(users.get(random.nextInt(users.size())));
+            posts.add(postRepository.save(post));
+        }
+
+        for (int i = 0; i < 150; i++) {
+            Comment comment = new Comment();
+            comment.setContent(faker.lorem().sentence(5));
+            comment.setPost(posts.get(random.nextInt(posts.size())));
+            comment.setAuthor(users.get(random.nextInt(users.size())));
+            commentRepository.save(comment);
+        }
+    }
+
+}
