3ddfe44201b8c37a263f098fdfc03bb7b783edea
JorshBG <jordiyair29@gmail.com>
2024-07-31 12:26:08 -0600

    Disable security for testing

--- src/main/java/com/jorshbg/practiceapispring/config/SecurityConfig.java
+++ src/main/java/com/jorshbg/practiceapispring/config/SecurityConfig.java
@@ -2,9 +2,11 @@ package com.jorshbg.practiceapispring.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -14,5 +16,13 @@ public class SecurityConfig {
     public PasswordEncoder encoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+                .securityMatcher("/api/v1/**")
+                .authorizeHttpRequests((requests) -> requests.anyRequest().permitAll());
+		return http.build();
+	}
 
 }
