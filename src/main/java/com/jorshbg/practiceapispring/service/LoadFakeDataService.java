5a8b93f5fa139a98559c7a4b818f752e89d5b06f
JorshBG <jordiyair29@gmail.com>
2024-08-01 22:41:22 -0600

    un-activate faker loader

--- src/main/java/com/jorshbg/practiceapispring/service/LoadFakeDataService.java
+++ src/main/java/com/jorshbg/practiceapispring/service/LoadFakeDataService.java
@@ -18,26 +18,26 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-@Service
+//@Service
 public class LoadFakeDataService {
 
-    @Autowired
+//    @Autowired
     private PasswordEncoder encoder;
 
     private Faker faker = new Faker();
     private Random random = new Random();
 
-    @Autowired
+//    @Autowired
     private UserRepository userRepository;
 
-    @Autowired
+//    @Autowired
     private PostRepository postRepository;
 
-    @Autowired
+//    @Autowired
     private CommentRepository commentRepository;
 
-    @PostConstruct
-    @Transactional
+//    @PostConstruct
+//    @Transactional
     public void init(){
         userRepository.deleteAll();
         postRepository.deleteAll();
