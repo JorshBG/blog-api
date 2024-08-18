b1c89583a038257d4042beb49b323b4430dda751
JorshBG <jordiyair29@gmail.com>
2024-08-18 16:35:10 -0600

    fix findByIdAndActive method to return an Optional object

--- src/main/java/com/jorshbg/practiceapispring/repository/IBlogDefaultRepository.java
+++ src/main/java/com/jorshbg/practiceapispring/repository/IBlogDefaultRepository.java
@@ -3,11 +3,15 @@ package com.jorshbg.practiceapispring.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
 
+import java.util.Optional;
+
+@NoRepositoryBean
 public interface IBlogDefaultRepository<E, I> extends JpaRepository<E, I> {
 
     Page<E> findByActive(Boolean active, Pageable pageable);
 
-    E findByIdAndActive(I id, Boolean active);
+    Optional<E> findByIdAndActive(I id, Boolean active);
 
 }
