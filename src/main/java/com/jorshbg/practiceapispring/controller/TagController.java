a0676267b878ff76763ce02f436436629965c126
JorshBG <jordiyair29@gmail.com>
2024-08-18 16:38:11 -0600

    fix loggers and exception management

--- src/main/java/com/jorshbg/practiceapispring/controller/TagController.java
+++ src/main/java/com/jorshbg/practiceapispring/controller/TagController.java
@@ -1,16 +1,17 @@
 package com.jorshbg.practiceapispring.controller;
 
 import com.jorshbg.practiceapispring.controller.interfaces.IControllerGeneric;
+import com.jorshbg.practiceapispring.dto.requests.TagDTO;
 import com.jorshbg.practiceapispring.dto.responses.ApiEntityResponse;
 import com.jorshbg.practiceapispring.dto.responses.PagedResponse;
 import com.jorshbg.practiceapispring.dto.responses.TagResponse;
 import com.jorshbg.practiceapispring.exception.BlogException;
-import com.jorshbg.practiceapispring.model.Tag;
 import com.jorshbg.practiceapispring.service.TagService;
 import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -28,66 +29,88 @@ public class TagController implements IControllerGeneric<TagResponse, Long> {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(TagController.class);
 
+    @Autowired
     private HttpServletRequest request;
 
     @GetMapping
     @Override
-    public ResponseEntity<PagedResponse<TagResponse>> getAll(@RequestParam(value = "page", defaultValue = "1") int page, @RequestParam(value = "size", defaultValue = "10") int size) {
+    public ResponseEntity<PagedResponse<TagResponse>> getActiveRecords(
+            @RequestParam(value = "page", defaultValue = "1") int page,
+            @RequestParam(value = "size", defaultValue = "10") int size
+    ) {
         LOGGER.info("Attempt to get all tags. Mapping: {}", request.getPathInfo());
-        var response = tagService.getAll(page, size);
+        var response = tagService.getAllActiveRecords(page, size, Sort.Direction.ASC, "id");
         LOGGER.info("Query finished");
         return ResponseEntity.ok(response);
     }
 
     @GetMapping("/{id}")
     @Override
-    public ResponseEntity<ApiEntityResponse<TagResponse>> getOne(@PathVariable Long id) {
+    public ResponseEntity<ApiEntityResponse<TagResponse>> getActiveById(@PathVariable Long id) {
         LOGGER.info("Attempt to get a tag with ID: {}. Mapping: {}", id, request.getPathInfo());
-        var response = tagService.getById(id);
+        var response = tagService.getActiveRecordById(id);
         LOGGER.info("Query finished");
         return ResponseEntity.ok(response);
     }
 
     @PostMapping
-    public ResponseEntity<ApiEntityResponse<TagResponse>> store(@Validated @RequestBody Tag tagRequest) throws URISyntaxException {
+    public ResponseEntity<ApiEntityResponse<TagResponse>> store(@Validated @RequestBody TagDTO tagRequest) {
         LOGGER.info("Attempt to store a tag. Mapping: {}. RequestBody: {}", request.getPathInfo(), tagRequest);
         var response = tagService.save(tagRequest);
-        var uriResource = request.getServletPath() + "/" + response.getData().getId();
+        URI uriResource = null;
+        try {
+            uriResource = new URI(request.getServletPath() + "/" + response.getData().getId());
+        } catch (URISyntaxException e) {
+            throw new BlogException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage(), e);
+        }
         LOGGER.info("Insertion finished. Response URI: {}", uriResource);
-        return ResponseEntity.created(new URI(uriResource)).body(response);
+        return ResponseEntity.created(uriResource).body(response);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<ApiEntityResponse<TagResponse>> update(@PathVariable Long id, @Validated @RequestBody Tag tagRequest) {
-        LOGGER.info("Attempt to update a tag. Mapping: {}, Method: {}. RequestBody: {}", request.getPathInfo(), request.getMethod(), tagRequest);
-        try {
-            var response = tagService.update(tagRequest);
-            LOGGER.info("Put update finished");
-            return ResponseEntity.ok(response);
-        } catch (IllegalAccessException e) {
-            throw new BlogException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage(), e);
-        }
+    public ResponseEntity<ApiEntityResponse<TagResponse>> update(
+            @PathVariable Long id,
+            @Validated @RequestBody TagDTO tagRequest
+    ) {
+        LOGGER.info("Attempt to update a tag. Mapping: {}, Method: {}. RequestBody: {}",
+                request.getPathInfo(),
+                request.getMethod(),
+                tagRequest);
+        var response = tagService.update(id, tagRequest);
+        LOGGER.info("Put update finished");
+        return ResponseEntity.ok(response);
     }
 
     @DeleteMapping("/{id}")
     @Override
     public ResponseEntity<ApiEntityResponse<TagResponse>> down(@PathVariable Long id) {
-        LOGGER.info("Attempt to delete a tag with ID: {}", id);
-        tagService.delete(id);
+        LOGGER.info("Attempt to deactivate a tag with ID: {}", id);
+        tagService.deactivate(id);
         LOGGER.info("Deleting finished");
         return ResponseEntity.noContent().build();
     }
 
+    @Override
+    @PatchMapping("/activate/{id}")
+    public ResponseEntity<ApiEntityResponse<TagResponse>> up(@PathVariable Long id) {
+        LOGGER.info("Attempt to activate a tag with ID: {}", id);
+        var response = tagService.activate(id);
+        LOGGER.info("Activate finished");
+        return ResponseEntity.ok(response);
+    }
+
     @PatchMapping("/{id}")
-    public ResponseEntity<ApiEntityResponse<TagResponse>> patchUpdate(@PathVariable Long id, @Validated @RequestBody Tag tagRequest) {
-        LOGGER.info("Attempt to update a tag. Mapping: {}, Method: {}. RequestBody: {}", request.getPathInfo(), request.getMethod(), tagRequest);
-        try {
-            var response = tagService.update(tagRequest);
-            LOGGER.info("Patch update finished");
-            return ResponseEntity.ok(response);
-        } catch (IllegalAccessException e) {
-            throw new BlogException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage(), e);
-        }
+    public ResponseEntity<ApiEntityResponse<TagResponse>> patchUpdate(
+            @PathVariable Long id,
+            @Validated @RequestBody TagDTO tagRequest
+    ) {
+        LOGGER.info("Attempt to update a tag. Mapping: {}, Method: {}. RequestBody: {}",
+                request.getPathInfo(),
+                request.getMethod(),
+                tagRequest);
+        var response = tagService.update(id, tagRequest);
+        LOGGER.info("Patch update finished");
+        return ResponseEntity.ok(response);
     }
 
 }
--- src/main/java/com/jorshbg/practiceapispring/service/TagService.java
+++ src/main/java/com/jorshbg/practiceapispring/service/TagService.java
@@ -1,5 +1,6 @@
 package com.jorshbg.practiceapispring.service;
 
+import com.jorshbg.practiceapispring.dto.requests.TagDTO;
 import com.jorshbg.practiceapispring.dto.responses.ApiEntityResponse;
 import com.jorshbg.practiceapispring.dto.responses.PagedResponse;
 import com.jorshbg.practiceapispring.dto.responses.TagResponse;
@@ -25,7 +26,7 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 @Service
-public class TagService implements IServiceGeneric<TagResponse, Long, Tag> {
+public class TagService implements IServiceGeneric<TagResponse, Long, TagDTO> {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(TagService.class);
 
@@ -36,29 +37,41 @@ public class TagService implements IServiceGeneric<TagResponse, Long, Tag> {
     private HttpServletRequest request;
 
     @Override
-    public ApiEntityResponse<TagResponse> getById(Long id) throws ResponseStatusException {
+    public ApiEntityResponse<TagResponse> getActiveRecordById(Long id) throws ResponseStatusException {
         LOGGER.info("Getting tag by id: {}", id);
-        Tag tag = repositoryTag.findById(id).orElseThrow(() -> new BlogException(HttpStatus.NOT_FOUND, "Tag with id: " + id + " not found"));
+        Tag tag = repositoryTag.findByIdAndActive(id, true).orElseThrow(() ->
+                new BlogException(HttpStatus.NOT_FOUND, "Tag with id: " + id + " not found"));
         LOGGER.info("Tag found: {}", tag);
         TagResponse response = TagMapper.INSTANCE.toResponse(tag);
         LOGGER.info("Tag converted to response");
-        return ApiResponseUtility.getResponse(response, this.selfLink());
+        return new ApiResponseUtility().getResponse(response,
+                new LinkedHashMap<>(Map.of("self", this.getServletPath() + "/" + id))
+        );
     }
 
     @Override
-    public ApiEntityResponse<TagResponse> save(Tag entity) throws ResponseStatusException {
+    public ApiEntityResponse<TagResponse> save(TagDTO entity) throws ResponseStatusException {
+        entity.setName(entity.getName().toLowerCase());
         LOGGER.info("Creating new resource of tag, Data entry: {}", entity);
-        Tag tag = repositoryTag.saveAndFlush(entity);
+        if (repositoryTag.findByName(entity.getName().toLowerCase()).isPresent()) {
+            throw new BlogException(HttpStatus.CONFLICT, "Tag with name: " + entity.getName() + " already exists");
+        }
+        Tag tag = repositoryTag.saveAndFlush(TagMapper.INSTANCE.fromDTO(entity));
         LOGGER.info("Tag created successfully with id: {}", tag.getId());
+        LOGGER.info("Tag created: {}", tag);
         TagResponse response = TagMapper.INSTANCE.toResponse(tag);
         LOGGER.info("Tag converted to response");
-        return ApiResponseUtility.getResponse(response, this.selfLink());
+        LOGGER.info("Response: {}", response);
+        return new ApiResponseUtility().getResponse(response,
+                new LinkedHashMap<>(Map.of("self", this.getServletPath() + "/" + response.getId()))
+        );
     }
 
     @Override
-    public void delete(Long id) throws ResponseStatusException {
+    public void deactivate(Long id) throws ResponseStatusException {
         LOGGER.info("The tag with id: {} is setting to down in 'active' property", id);
-        Tag tag = repositoryTag.findById(id).orElseThrow(() -> new BlogException(HttpStatus.NOT_FOUND, "Tag with id: " + id + " not found"));
+        Tag tag = repositoryTag.findById(id).orElseThrow(() ->
+                new BlogException(HttpStatus.NOT_FOUND, "Tag with id: " + id + " not found"));
         LOGGER.info("Tag found: {}", tag);
         tag.setActive(false);
         repositoryTag.saveAndFlush(tag);
@@ -66,40 +79,70 @@ public class TagService implements IServiceGeneric<TagResponse, Long, Tag> {
     }
 
     @Override
-    public ApiEntityResponse<TagResponse> update(Tag update) throws ResponseStatusException, IllegalAccessException {
-        LOGGER.info("Updating tag with id: {}", update.getId());
+    public ApiEntityResponse<TagResponse> activate(Long id) throws ResponseStatusException {
+        LOGGER.info("The tag with id: {} is setting to activate", id);
+        Tag tag = repositoryTag.findById(id).orElseThrow(() ->
+                new BlogException(HttpStatus.NOT_FOUND, "Tag with id: " + id + " not found"));
+        LOGGER.info("Tag found: {}", tag);
+        tag.setActive(true);
+        repositoryTag.saveAndFlush(tag);
+        LOGGER.info("Active property is true state now");
+        TagResponse response = TagMapper.INSTANCE.toResponse(tag);
+        LOGGER.info("Tag converted to response");
+        return new ApiResponseUtility().getResponse(response,
+                new LinkedHashMap<>(Map.of("self", this.getServletPath() + "/" + response.getId()))
+        );
+    }
+
+    @Override
+    public ApiEntityResponse<TagResponse> update(Long id, TagDTO update) throws ResponseStatusException {
+        LOGGER.info("Updating tag");
         LOGGER.info("Entry data: {}", update);
-        Tag tag = repositoryTag.findById(update.getId()).orElseThrow(() -> new BlogException(HttpStatus.NOT_FOUND, "Tag with id: " + update.getId() + " not found"));
+        Tag tag = repositoryTag.findById(id).orElseThrow(() ->
+                new BlogException(HttpStatus.NOT_FOUND, "Tag with id: " + id + " not found"));
         LOGGER.info("Tag found: {}. Starting with mapping", tag);
+        LOGGER.info("Converting tag founded to auxiliar object to check field updates");
+        Tag copyToCheckUpdate = new Tag();
+        TagMapper.INSTANCE.copy(tag, copyToCheckUpdate);
+        LOGGER.info("Successful copy. Starting to update tag");
         TagMapper.INSTANCE.update(update, tag);
+        repositoryTag.saveAndFlush(tag);
+        LOGGER.info("Tag updated. Starting to search changes.");
         UpdatesTrackerUtil<Tag> tracker = new UpdatesTrackerUtil<>();
-        var changes = tracker.getUpdates(tag, update);
+        Map<String, Map<String, String>> changes = null;
+        try {
+            changes = tracker.getUpdates(copyToCheckUpdate, tag);
+        } catch (IllegalAccessException e) {
+            LOGGER.error("Unable to catch changes of the entity. Error: {}", e.getMessage(), e);
+        }
         LOGGER.info("Mapping finished. Fields updated: {}", changes);
         TagResponse response = TagMapper.INSTANCE.toResponse(tag);
         LOGGER.info("Tag converted to response");
-        return ApiResponseUtility.getResponse(response, this.selfLink());
+        return new ApiResponseUtility().getResponse(response, this.selfLink());
     }
+
 
     @Override
-    public PagedResponse<TagResponse> getAll(int page, int size) throws ResponseStatusException {
-        Page<Tag> paginated = repositoryTag.findAll(PageRequest.of(page, size));
-        return ApiResponseUtility.getPagedResponse(this.convert(page, size, paginated), paginated, "tags");
-    }
-
-    @Override
-    public PagedResponse<TagResponse> getAll(int page, int size, Sort.Direction direction, String orderBy) throws ResponseStatusException {
-        Page<Tag> paginated = repositoryTag.findAll(PageRequest.of(page, size, direction, orderBy));
-        return ApiResponseUtility.getPagedResponse(this.convert(page, size, paginated), paginated, "tags");
-    }
-
-    private Iterable<TagResponse> convert(int page, int size, Page<Tag> paginated) {
+    public PagedResponse<TagResponse> getAllActiveRecords(int page, int size, Sort.Direction direction, String orderBy) throws ResponseStatusException {
+        page = Math.max(page - 1, 0);
+        Page<Tag> paginated = repositoryTag.findByActive(true, PageRequest.of(page, size, direction, orderBy));
+        if (!(page < paginated.getTotalPages())) {
+            throw new BlogException(HttpStatus.NOT_FOUND, "No page found");
+        }
+        if (!paginated.hasContent()) {
+            throw new BlogException(HttpStatus.NOT_FOUND, "No data found");
+        }
         LOGGER.info("Query complete to paginate. Page: {}, Size: {}, Total amount data: {}", page, size, paginated.getTotalElements());
         Iterable<TagResponse> tags = TagMapper.INSTANCE.toResponses(paginated.getContent());
         LOGGER.info("Conversion to TagResponse complete");
-        return tags;
+        return new ApiResponseUtility().getPagedResponse(tags, paginated, "tags");
     }
 
-    private LinkedHashMap<String, String> selfLink(){
-        return new LinkedHashMap<>(Map.of("self", request.getPathInfo()));
+    private String getServletPath(){
+        return request.getContextPath() + request.getServletPath();
+    }
+
+    private LinkedHashMap<String, String> selfLink() {
+        return new LinkedHashMap<>(Map.of("self", request.getContextPath() + request.getServletPath()));
     }
 }
