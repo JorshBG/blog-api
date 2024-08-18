93f410b2a9a59da702c50e6464212ac482967271
JorshBG <jordiyair29@gmail.com>
2024-08-18 11:33:26 -0600

    Fix entity generic param on save and update

--- src/main/java/com/jorshbg/practiceapispring/service/interfaces/IServiceGeneric.java
+++ src/main/java/com/jorshbg/practiceapispring/service/interfaces/IServiceGeneric.java
@@ -2,18 +2,26 @@ package com.jorshbg.practiceapispring.service.interfaces;
 
 import com.jorshbg.practiceapispring.dto.responses.ApiEntityResponse;
 import com.jorshbg.practiceapispring.dto.responses.PagedResponse;
+import com.jorshbg.practiceapispring.dto.responses.TagResponse;
+import com.jorshbg.practiceapispring.model.Tag;
 import org.springframework.data.domain.Sort;
 import org.springframework.web.server.ResponseStatusException;
 
-public interface IServiceGeneric<T, E> {
+/**
+ * Generic Service to implement in new Blog service to access to db resources
+ * @param <T> Response type
+ * @param <E> ID type of the model
+ * @param <R> Request type
+ */
+public interface IServiceGeneric<T, E, R> {
 
     ApiEntityResponse<T> getById(E id) throws ResponseStatusException;
 
-    ApiEntityResponse<T> save(T entity) throws ResponseStatusException;
+    ApiEntityResponse<T> save(R entity) throws ResponseStatusException;
 
     void delete(E id) throws ResponseStatusException;
 
-    ApiEntityResponse<T> update(T entity) throws ResponseStatusException;
+    ApiEntityResponse<T> update(R entity) throws ResponseStatusException, IllegalAccessException;
 
     PagedResponse<T> getAll(int page, int size) throws ResponseStatusException;
 
