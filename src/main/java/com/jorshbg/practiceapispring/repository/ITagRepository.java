5e15adfd64c44d09f8f13c5881134b16aedae39f
JorshBG <jordiyair29@gmail.com>
2024-08-18 11:34:18 -0600

    change parent Repository

--- src/main/java/com/jorshbg/practiceapispring/repository/ITagRepository.java
+++ src/main/java/com/jorshbg/practiceapispring/repository/ITagRepository.java
@@ -1,9 +1,11 @@
 package com.jorshbg.practiceapispring.repository;
 
 import com.jorshbg.practiceapispring.model.Tag;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ITagRepository extends JpaRepository<Tag, Integer> {
+public interface ITagRepository extends IBlogDefaultRepository<Tag, Long>{
 }
