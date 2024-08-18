3797481a77a8600caf1141270ecd7b6b2767487a
JorshBG <jordiyair29@gmail.com>
2024-08-18 12:00:48 -0600

    Add generic controller interface

---
+++ src/main/java/com/jorshbg/practiceapispring/controller/interfaces/IControllerGeneric.java
@@ -0,0 +1,18 @@
+package com.jorshbg.practiceapispring.controller.interfaces;
+
+import com.jorshbg.practiceapispring.dto.responses.ApiEntityResponse;
+import com.jorshbg.practiceapispring.dto.responses.PagedResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+public interface IControllerGeneric<E, I> {
+
+    ResponseEntity<PagedResponse<E>> getAll(@RequestParam(value = "page", defaultValue = "1") int page, @RequestParam(value = "size", defaultValue = "10") int size);
+
+    ResponseEntity<ApiEntityResponse<E>> getOne(@PathVariable I id);
+
+    ResponseEntity<ApiEntityResponse<E>> down(@PathVariable I id);
+
+}
