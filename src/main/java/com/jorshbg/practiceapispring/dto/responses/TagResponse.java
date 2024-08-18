3918d3101304b082a832924dee999de7615db70e
JorshBG <jordiyair29@gmail.com>
2024-08-18 16:37:21 -0600

    Remove @data annotation and add set and get methods

--- src/main/java/com/jorshbg/practiceapispring/dto/responses/TagResponse.java
+++ src/main/java/com/jorshbg/practiceapispring/dto/responses/TagResponse.java
@@ -1,11 +1,24 @@
 package com.jorshbg.practiceapispring.dto.responses;
 
-import lombok.Data;
-
-@Data
 public class TagResponse {
 
     private Long id;
 
     private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
