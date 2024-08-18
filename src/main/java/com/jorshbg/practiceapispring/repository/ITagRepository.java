879adf8fbc55e9cf2d5e266ba0e9107c653584a9
JorshBG <jordiyair29@gmail.com>
2024-08-18 16:31:56 -0600

    Add findByName method

--- src/main/java/com/jorshbg/practiceapispring/repository/ITagRepository.java
+++ src/main/java/com/jorshbg/practiceapispring/repository/ITagRepository.java
@@ -6,6 +6,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ITagRepository extends IBlogDefaultRepository<Tag, Long>{
+
+    Optional<Tag> findByName(String name);
+
 }
