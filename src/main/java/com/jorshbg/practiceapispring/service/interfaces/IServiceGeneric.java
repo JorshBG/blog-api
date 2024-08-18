dea776080c10d16133993a21b183175d6f6cc82a
JorshBG <jordiyair29@gmail.com>
2024-08-18 16:32:23 -0600

    rename methods

--- src/main/java/com/jorshbg/practiceapispring/service/interfaces/IServiceGeneric.java
+++ src/main/java/com/jorshbg/practiceapispring/service/interfaces/IServiceGeneric.java
@@ -2,29 +2,27 @@ package com.jorshbg.practiceapispring.service.interfaces;
 
 import com.jorshbg.practiceapispring.dto.responses.ApiEntityResponse;
 import com.jorshbg.practiceapispring.dto.responses.PagedResponse;
-import com.jorshbg.practiceapispring.dto.responses.TagResponse;
-import com.jorshbg.practiceapispring.model.Tag;
 import org.springframework.data.domain.Sort;
 import org.springframework.web.server.ResponseStatusException;
 
 /**
  * Generic Service to implement in new Blog service to access to db resources
  * @param <T> Response type
- * @param <E> ID type of the model
+ * @param <ID> ID type of the model
  * @param <R> Request type
  */
-public interface IServiceGeneric<T, E, R> {
+public interface IServiceGeneric<T, ID, R> {
 
-    ApiEntityResponse<T> getById(E id) throws ResponseStatusException;
+    ApiEntityResponse<T> getActiveRecordById(ID id) throws ResponseStatusException;
 
     ApiEntityResponse<T> save(R entity) throws ResponseStatusException;
 
-    void delete(E id) throws ResponseStatusException;
+    void deactivate(ID id) throws ResponseStatusException;
 
-    ApiEntityResponse<T> update(R entity) throws ResponseStatusException, IllegalAccessException;
+    ApiEntityResponse<T> activate(ID id) throws ResponseStatusException;
 
-    PagedResponse<T> getAll(int page, int size) throws ResponseStatusException;
+    ApiEntityResponse<T> update(ID id, R entity) throws ResponseStatusException, IllegalAccessException;
 
-    PagedResponse<T> getAll(int page, int size, Sort.Direction direction, String orderBy) throws ResponseStatusException;
+    PagedResponse<T> getAllActiveRecords(int page, int size, Sort.Direction direction, String orderBy) throws ResponseStatusException;
 
 }
