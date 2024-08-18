589a2de3c1ffa00471d5817614825b3e9980759f
JorshBG <jordiyair29@gmail.com>
2024-08-18 16:37:03 -0600

    Remove @data annotation and add set and get methods

--- src/main/java/com/jorshbg/practiceapispring/model/Tag.java
+++ src/main/java/com/jorshbg/practiceapispring/model/Tag.java
@@ -1,9 +1,12 @@
 package com.jorshbg.practiceapispring.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
+import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.validator.constraints.UniqueElements;
 
 import java.io.Serial;
 import java.io.Serializable;
@@ -11,7 +14,6 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
-@Data
 @Table(name = "tags")
 public class Tag implements Serializable {
 
@@ -23,9 +25,12 @@ public class Tag implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Size(min = 2, max = 60)
+    @Column(unique = true, nullable = false)
     private String name;
 
-    private Boolean active;
+    @ColumnDefault(value = "true")
+    private Boolean active = true;
     //endregion
 
     //region Relationships
@@ -43,4 +48,52 @@ public class Tag implements Serializable {
     private LocalDateTime lastModifiedAt;
     //endregion
 
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public @Size(min = 2, max = 60) String getName() {
+        return name;
+    }
+
+    public void setName(@Size(min = 2, max = 60) String name) {
+        this.name = name;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getLastModifiedAt() {
+        return lastModifiedAt;
+    }
+
+    public void setLastModifiedAt(LocalDateTime lastModifiedAt) {
+        this.lastModifiedAt = lastModifiedAt;
+    }
 }
